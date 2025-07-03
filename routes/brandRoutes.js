ac2ac618b9166c2cc0ad3abec852781e66f7d7ac
riyan <riyanarhm28@gmail.com>
2025-07-03 20:14:20 +0700

    fixing

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -1,3 +1,4 @@
+// routes/brandRoutes.js
 const express = require('express');
 const multer = require('multer');
 const BrandController = require('../controllers/brandController');
@@ -6,8 +7,5 @@ const router = express.Router();
 const upload = multer(); // memoryStorage
 
 router.post('/create', upload.single('logo'), BrandController.createBrand);
-
-const imageCID = await PinataService.uploadFile(file.buffer, 'nama_file_logo');
-const jsonCID = await PinataService.uploadJSON(metadata, 'nama_metadata.json');
 
 module.exports = router;
