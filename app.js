b650f6fc96d851561239373903094fec904eaedd
riyan <riyanarhm28@gmail.com>
2025-07-03 15:05:10 +0700

    fixing server.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+// app.js
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-require('dotenv').config();
 
 const nftRoutes = require('./routes/nftRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const verifyRoutes = require('./routes/verifyRoutes');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// setup route
+app.use('/api', nftRoutes);
+app.use('/api', uploadRoutes);
+app.use('/api', verifyRoutes);
+
+module.exports = app;
