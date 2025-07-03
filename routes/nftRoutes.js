e6f11847bb925c363aedbdbee21a931f360139fd
riyan <riyanarhm28@gmail.com>
2025-07-03 12:58:53 +0700

    creating GET NFT 2

--- routes/nftRoutes.js
+++ routes/nftRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { mintNFT } = require('../controllers/nftController');
+const { mintNFT, getAllNFTs } = require('../controllers/nftController');
 
 const router = express.Router();
 
 router.post('/mint', mintNFT);
+router.get('/nfts', getAllNFTs); 
+
 
 module.exports = router;
