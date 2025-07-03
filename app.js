82b00b6254d398531da6dd59b9775853365a5366
riyan <riyanarhm28@gmail.com>
2025-07-03 10:59:46 +0700

    connect to Supabase

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const nftRoutes = require('./routes/nftRoutes');
+const nftRoutes = require('./routes/nftRoutes'); // pastikan path-nya benar
 
 const app = express();
 app.use(express.json());
-
 app.use('/api', nftRoutes); 
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); 
+
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
