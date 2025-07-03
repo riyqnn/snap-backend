cd297c68bf67a64643b8205b1b3db9bb2eacf019
riyan <riyanarhm28@gmail.com>
2025-07-03 11:11:37 +0700

    fixing supabase

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -7,7 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }, 
+  ssl: {
+    rejectUnauthorized: false
+  },
 });
 
 module.exports = pool;
