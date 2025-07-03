2b50cffa29cbf964b7aab5d28a26a4602864430e
riyan <riyanarhm28@gmail.com>
2025-07-03 11:27:45 +0700

    fixing render to connect supabase

--- database/db.js
+++ database/db.js
@@ -6,9 +6,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT), 
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false, 
   },
 });
 
